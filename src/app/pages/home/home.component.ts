162b369e00b8f843b83d3679b0e744b4b9fee339
Antonio T <h.teran@utp.edu.co>
2023-06-28 20:24:01 -0500

    Home styles & content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  cards = [
+    {
+      picture: "assets/water-solid 1.png",
+      title: "Versatility",
+      content: "The range of flavors in gin is exceptionally wide, thanks to the variety of botanicals that can be used in its production."
+    },
+    {
+      picture: "assets/cocktail-solid 1.png",
+      title: "Distiontiv Flavor",
+      content: "Bittersweet and slightly woody flavor is the signature of any gin."
+    },
+    {
+      picture: "assets/Vector.png",
+      title: "Purity",
+      content: "This gives it a clean, pure feel, without the caramel or vanilla notes that come from barrel aging."
+    }
+  ];
 
   constructor() { }
 
